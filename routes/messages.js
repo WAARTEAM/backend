79709d4d4d5261c292843bec01c39fe8a340939d
Noah <ahmedehsouna@yahoo.com>
2020-01-18 19:37:40 +0100

    Im writin alot of commits just because of heroku so omar just ignore em plz ok?

--- routes/messages.js
+++ routes/messages.js
@@ -3,7 +3,6 @@ const Message = require("../models/message");
 const AUTH = require("passport").authenticate("jwt", { session: false });
 
 Router.route("/latest").get(AUTH, (req, res) => {
-
   Message.aggregate([
     { $match: { sender: req.user._id } },
     { $group: { _id: "$receiver", msgId: { $last: "$_id" } } }
@@ -12,31 +11,36 @@ Router.route("/latest").get(AUTH, (req, res) => {
       { $match: { receiver: req.user._id } },
       { $group: { _id: "$sender", msgId: { $last: "$_id" } } }
     ]).exec((err, secondIds) => {
-        // Message.find()
-        Message.find({$or : [...firstIds,...secondIds].map(one =>{
-            return {_id : one.msgId}
-        })}).sort({_id : -1}).populate(["sender" , "receiver"]).exec((err,wow)=>{
-                    var unique = {}  
-                    var latest = []
-                    wow.forEach(one => {
-        
-                        if (one.sender.username == req.user.username){
-                           if(!unique[one.receiver.username]){
-                               unique[one.receiver.username] = true
-                               latest.push(one)
-
-                           }
-                        }else{
-                            if(!unique[one.sender.username]){
-                                unique[one.sender.username] = true
-                                latest.push(one)
- 
-                            }                        }   
-                         
-                    })
-                    res.json(latest)
-            })
-
+      // Message.find()
+      Message.find({
+        $or: [...firstIds, ...secondIds].map(one => {
+          return { _id: one.msgId };
+        })
+      })
+        .sort({ _id: -1 })
+        .populate(["sender", "receiver"])
+        .exec((err, wow) => {
+          var unique = {};
+          var latest = [];
+          wow.forEach(one => {
+            if (one.sender.username == req.user.username) {
+              if (one.receiver) {
+              if (!unique[one.receiver.username]) {
+                  unique[one.receiver.username] = true;
+                  latest.push(one);
+                }
+              }
+            } else {
+              if (one.sender) {
+                if (!unique[one.sender.username]) {
+                  unique[one.sender.username] = true;
+                  latest.push(one);
+                }
+              }
+            }
+          });
+          res.json(latest);
+        });
     });
   });
   // Message.find({sender : req.user._id}).distinct("receiver").exec((err,partOne)=>{
